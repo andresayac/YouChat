ba4a292715122a5ae3b61d6b18d0a92eec0fb5cc
Andres Aya <andresayac@gmail.com>
2022-12-26 10:48:39 -0500

    Activo headless

--- app.php
+++ app.php
@@ -6,8 +6,7 @@ require_once 'inc/You.class.php';
 
 $you = new You();
 
-$question_answer = $you->askQuestion("Puedes programar en php un programa que imprima el arbol de navidad con simbolos", "");
-
+$question_answer = $you->askQuestion("Puedes desarrollar un formulario en html que consulte un api con jquery y la respuesta la muestre en un div", "");
 $data_processed = $you->process_data($question_answer);
 
 foreach ($data_processed  as $key => $value) {
--- inc/You.class.php
+++ inc/You.class.php
@@ -12,9 +12,9 @@ class You
     {
         $browserFactory = new BrowserFactory();
         $this->browser = $browserFactory->createBrowser([
-            'headless' => false, // disable headless mode
+            'headless' => true, // disable headless mode
             'keepAlive' => true,
-            'noSandbox' => true,
+            'userAgent' => 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0'
         ]);
     }
 
@@ -25,19 +25,19 @@ class You
 
         // creates a new page and navigate to an URL
         $page =  $this->browser->createPage();
-        $page->navigate($query)->waitForNavigation(Page::NETWORK_IDLE);
-      
+        $page->navigate($query)->waitForNavigation(Page::DOM_CONTENT_LOADED);
 
         // get data
         $body = $page->evaluate('document.querySelector("body").innerHTML')->getReturnValue();
         return $body;
     }
 
-    public function process_data($data){
-        $my_data = explode("\nevent: token\ndata: ",$data);
-        $process_data = [];    
+    public function process_data($data)
+    {
+        $my_data = explode("\nevent: token\ndata: ", $data);
+        $process_data = [];
         foreach ($my_data as $value) {
-            if(json_decode($value)){
+            if (json_decode($value)) {
                 $process_data[] = json_decode($value);
             }
         }
